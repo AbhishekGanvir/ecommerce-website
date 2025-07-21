523aeee7793f412c626d3a3cdc11b246a5f4e5d5
Abhishek Ganvir <abhishekganvirr@gmail.com>
2025-07-21 22:20:16 +0530

    add to cart interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -46,11 +46,41 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary  js-add-to-cart"
+             data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`
     });
-    console.log(productsHTML);
 
+    // querySelector
+//Returns: The first element that matches the specified CSS selector.
     document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+    //querySelectorAll
+//Returns: All elements that match the specified CSS selector.
+    document.querySelectorAll('.js-add-to-cart').forEach( (button) => {
+    button.addEventListener('click', () => { 
+       const productId = button.dataset.productId;
+
+       let matchingItem;
+       //check if product is in the cart
+       cart.forEach((item) => {
+        if(productId === item.productId){
+            matchingItem = item;
+        }
+       });
+       //it it is in cart,increase the quantity
+       if (matchingItem){
+        matchingItem.quantity +=1;
+       }
+       // if it is not in cart, add in cart
+           else{
+            cart.push({
+        productId: productId,
+        quantity:1
+       });
+     } 
+       console.log(cart);
+      });
+    });
