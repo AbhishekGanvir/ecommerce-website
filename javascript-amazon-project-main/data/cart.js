6ee21d2d723ea0cd5a9ffed8f91b4362e77b2bea
Abhishek Ganvir <abhishekganvirr@gmail.com>
2025-07-22 20:36:22 +0530

    fix price and currency

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,1 +1,29 @@
-export const cart = [];
+export const cart = [{
+    productId:`e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,
+    quantity:2,
+},{
+     productId:`15b6fc6f-327a-4ec4-896f-486349e85a3d`,
+     quantity:1
+}        
+];
+
+export function addToCart(productId){
+    let matchingItem;
+       //check if product is in the cart
+       cart.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+       });
+       //it it is in cart,increase the quantity
+       if (matchingItem){
+        matchingItem.quantity +=1;
+       }
+       // if it is not in cart, add in cart
+           else{
+            cart.push({
+        productId: productId,
+        quantity:1
+       });
+     } 
+}
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,7 +1,9 @@
-import { cart } from "../data/cart.js";
+import { cart,addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
-
+import { formatCurrency } from "../utils/money.js";
 let productsHTML = '';
+
+
 
 products.forEach((product) => {
    productsHTML += `<div class="product-container">
@@ -23,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            ₹${(product.priceCents / 8).toFixed(0) }
+            ₹${formatCurrency(product.priceCents) }
           </div>
 
           <div class="product-quantity-container">
@@ -55,41 +57,24 @@ products.forEach((product) => {
         </div>`
     });
 
-    // querySelector
-//Returns: The first element that matches the specified CSS selector.
+
     document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-    //querySelectorAll
-//Returns: All elements that match the specified CSS selector.
-    document.querySelectorAll('.js-add-to-cart').forEach( (button) => {
-    button.addEventListener('click', () => { 
-       const productId = button.dataset.productId;
-
-       let matchingItem;
-       //check if product is in the cart
-       cart.forEach((item) => {
-        if(productId === item.productId){
-            matchingItem = item;
-        }
-       });
-       //it it is in cart,increase the quantity
-       if (matchingItem){
-        matchingItem.quantity +=1;
-       }
-       // if it is not in cart, add in cart
-           else{
-            cart.push({
-        productId: productId,
-        quantity:1
-       });
-     } 
-
+  function updateCartQuantity(){   
      let cartQuantity = 0;
-     cart.forEach((item) => {
-      cartQuantity += item.quantity;
+     cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
      });
 
      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
        console.log(cart);
+  }
+
+
+    document.querySelectorAll('.js-add-to-cart').forEach( (button) => {
+    button.addEventListener('click', () => { 
+       const productId = button.dataset.productId;
+       addToCart(productId);
+       updateCartQuantity();
       });
     });
